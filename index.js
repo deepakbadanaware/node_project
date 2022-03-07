32044b8144a93b2e9055e45f43a4da6751a249b3
Rajshree1234 <39979090+Rajshree1234@users.noreply.github.com>
2022-03-07 20:49:05 +0530

    Update index.js

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ app.set('port', (process.env.PORT || 5000))
 app.use(express.static(__dirname + '/public'))
 
 app.get('/', function(request, response) {
-  response.send('Hello Testing DevOps Node! JS Project Hello How are you ? ')
+  response.send('Hello cliff.ai ! this is sample node application ')
 }) 
 
 app.listen(app.get('port'), function() {
