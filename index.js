63d315ba396eac3fa67518091b3809deea28386c
Deepak Badnaware <83689441+deepakbadanaware@users.noreply.github.com>
2022-03-07 20:53:28 +0530

    Update index.js

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ app.set('port', (process.env.PORT || 5000))
 app.use(express.static(__dirname + '/public'))
 
 app.get('/', function(request, response) {
-  response.send('Hello cliff.ai ! this is sample node application ')
+  response.send('Hello cliff.ai ! this is sample node application trying ')
 }) 
 
 app.listen(app.get('port'), function() {
